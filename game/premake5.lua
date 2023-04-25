f9f6ae3cebeddd93d6ca7ba90952304d2865e5f0
Jeff Myers <JeffM2501@gmail.com>
2023-04-24 17:23:45 -0700

    code folders

--- game/premake5.lua
+++ game/premake5.lua
@@ -1,31 +1,30 @@
 
 baseName = path.getbasename(os.getcwd());
 
-project (workspaceName)
-  	kind "ConsoleApp"
+project (baseName)
+    kind "ConsoleApp"
     location "../_build"
     targetdir "../_bin/%{cfg.buildcfg}"
-	
-    filter "configurations:Release"
-		kind "WindowedApp"
-		entrypoint "mainCRTStartup"
 
-	filter "action:vs*"
+    filter "action:vs*"
         debugdir "$(SolutionDir)"
 		
 	filter {"action:vs*", "configurations:Release"}
-			kind "WindowedApp"
-			entrypoint "mainCRTStartup"
-	filter {}
-	
-	vpaths 
-	{
-	  ["Header Files/*"] = { "include/**.h",  "include/**.hpp", "src/**.h", "src/**.hpp", "**.h", "**.hpp"},
-	  ["Source Files/*"] = {"src/**.c", "src/**.cpp","**.c", "**.cpp"},
-	}
-	files {"**.c", "**.cpp", "**.h", "**.hpp"}
+		kind "WindowedApp"
+		entrypoint "mainCRTStartup"
+		
+    filter{}
 
-	includedirs { "./", "src", "include"}
-	link_raylib();
-	
+    vpaths 
+    {
+        ["Header Files/*"] = { "include/**.h",  "include/**.hpp", "src/**.h", "src/**.hpp", "**.h", "**.hpp"},
+        ["Source Files/*"] = {"src/**.c", "src/**.cpp","**.c", "**.cpp"},
+    }
+    files {"**.c", "**.cpp", "**.h", "**.hpp"}
+  
+    includedirs { "./"}
+    includedirs {"src"}
+    includedirs {"include"}
+    link_raylib()
+	link_to("ecs")
 	-- To link to a lib use link_to("LIB_FOLDER_NAME")
